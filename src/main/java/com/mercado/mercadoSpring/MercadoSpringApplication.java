3774909cd94b87cdfc495a1bb7c6c81163ae23a5
asohyannick <asoh@cskytelecoms.com>
2025-11-12 20:25:37 +0100

    I have developed and setup the foundational structure for a scalable, robust, secure and highly function Restful API for an ecommerce application developed with Spring Boot

---
+++ src/main/java/com/mercado/mercadoSpring/MercadoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.mercadoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercado/mercadoSpring/MercadoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.mercadoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
